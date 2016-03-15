80c6a1792cc8f267a5db08d65bc9ebee864dc61f
Eric Simons <ericsimons@Erics-MacBook.local>
2016-03-15 12:59:38 -0700

    comments done

---
+++ src/js/article/comment.component.js
@@ -0,0 +1,25 @@
+class CommentCtrl {
+  constructor(User, Comments) {
+    'ngInject';
+
+    // The user can only edit/delete this comment if they are the author
+    if (User.current) {
+      this.canModify = (User.current.username === this.data.author.username);
+    } else {
+      this.canModify = false;
+    }
+
+  }
+
+}
+
+let Comment = {
+  bindings: {
+    data: '=',
+    deleteCb: '&'
+  },
+  controller: CommentCtrl,
+  templateUrl: 'article/comment.html'
+};
+
+export default Comment;
